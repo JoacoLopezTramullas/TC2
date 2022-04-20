3ab5c60cec67c973a516159a52a5ba3d1074f5cc
Joaquín López <jlpeztramullas@frba.utn.edu.ar>
2022-04-20 13:23:05 -0300

    TS1 terminada

--- TS1.py
+++ TS1.py
@@ -1,19 +1,19 @@
 import numpy as np
-import matplotlib as mpl
 from matplotlib import pyplot as plt
 
 # módulo de SciPy
 from scipy import signal as sig
 
 # Agrego funciones que voy a usar de splane
-from splane import bodePlot
+from splane import bodePlot, pzmap
 
 # Defino valores de componentes
 R1 = 1000
 R2 = 1000
+Rn = R2/R1
 
 # Defino numerador y denominador
-num = np.array([1,-R2/R1])
+num = np.array([1,-Rn])
 den = np.array([1,1])
 
 # Obtengo transferencia
@@ -22,4 +22,7 @@ H_s = sig.TransferFunction(num,den)
 # Ploteo
 plt.close('all')
 bodePlot(H_s, [-3,3], [-3,3])
+
+# Diagrama de polos y ceros
+pzmap(H_s)
 
